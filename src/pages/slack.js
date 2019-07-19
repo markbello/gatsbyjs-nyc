88d2f9af9526919c744fb2df8ca81fd34817196d
Edgar Ong <emailedgar@gmail.com>
2019-07-19 13:29:27 -0400

    create slack page

---
+++ src/pages/slack.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Layout from "../layout"
+
+export default function Slack() {
+  return (
+    <Layout>
+      <form>
+        <input type="text" />
+        <input type="button" value="Invite Me" />
+      </form>
+    </Layout>
+  )
+}
