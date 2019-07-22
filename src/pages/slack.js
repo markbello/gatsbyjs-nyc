db96ade965fa9e210a865988290e44f2fc757e6b
Edgar Ong <emailedgar@gmail.com>
2019-07-21 20:40:06 -0400

    refactored with mark

--- src/pages/slack.js
+++ src/pages/slack.js
@@ -5,29 +5,22 @@ import "./slack.css"
 import SEO from "../components/seo"
 
 export default function Slack() {
-  // HELPERS
   const [email, setEmail] = useState("")
   const [submitToast, setSubmitToast] = useState(false)
 
   const requestInvite = () => {
     setSubmitToast(true)
-    // setTimeout for form to process before resetting state
     setTimeout(() => setEmail(""), 1000)
   }
-
-  const displayToast = () => {
-    return (
-      <div id="toast" onClick={() => setSubmitToast(false)}>
-        Invite sent!
-      </div>
-    )
-  }
-  // end HELPERS
 
   return (
     <Layout>
       <SEO title="Gatsby NYC's Slack Community" />
-      {submitToast ? displayToast() : null}
+      {submitToast && (
+        <div id="toast" onClick={() => setSubmitToast(false)}>
+          Invite sent!
+        </div>
+      )}
       <div id="slack-form">
         <h1 style={{ textAlign: "center" }}>Gatsby NYC's Slack Community</h1>
         <p>
@@ -40,7 +33,7 @@ export default function Slack() {
           action="https://docs.google.com/forms/d/e/1FAIpQLSevJbIBx8CFxtYjL7aR4sfIIUlroouHsYUqXK2AqEcD2NwvyQ/formResponse"
           method="post"
           target="_blank"
-          onSubmit={() => requestInvite()}
+          onSubmit={requestInvite}
         >
           <input
             aria-label="Enter your email to be invited to Gatsy NYC's Slack channel."
@@ -50,7 +43,7 @@ export default function Slack() {
             placeholder="Your email"
             type="email"
             value={email}
-            onChange={e => setEmail(e.target.value)}
+            onChange={({ target: { value } }) => setEmail(value)}
           />
           <button aria-label="Submit your email." type="submit">
             Invite Me!
